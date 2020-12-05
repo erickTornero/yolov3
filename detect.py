23ef93e0a1f341d5d1653326b8479c5c0d8e8ebc
erickTornero <erickdeivy01@gmail.com>
2020-12-05 16:20:07 -0500

    [mod] Improve readme

--- detect.py
+++ detect.py
@@ -116,8 +116,11 @@ def detect(save_img=False):
             # Stream results
             if view_img:
                 cv2.imshow(str(p), im0)
-                if cv2.waitKey(1) == ord('q'):  # q to quit
-                    raise StopIteration
+                k = cv2.waitKey(0)
+                if k == 27:
+                    cv2.destroyAllWindows()
+                #if cv2.waitKey(1) == ord('q'):  # q to quit
+                #    raise StopIteration
 
             # Save results (image with detections)
             if save_img:
